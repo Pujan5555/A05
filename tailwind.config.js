99561eb757d5e2ae217d94fa432e29893a90f25a
Pujan5555 <puzanchakma@gmail.com>
2025-04-10 08:05:08 +0600

    second push

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    content: ["./index.html"],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+    theme: {
+        extend: {
+          fontFamily: {
+            manrope: ['Manrope', 'sans-serif'],
+          },
+        },
+      },
+      plugins: [],
+};
